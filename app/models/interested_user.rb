07d6a47f75191ef20b8e47d4f485c0e735e5ec1d
railskipl <rails@kunalinfotech.net>
2013-01-30 09:36:56 +0530

    changes in model

--- app/models/interested_user.rb
+++ app/models/interested_user.rb
@@ -1,3 +1,3 @@
 class InterestedUser < ActiveRecord::Base
-  # attr_accessible :title, :body
+   attr_accessible :user_id, :user_name, :birthday, :gender, :location, :contact_no, :movie_id, :movie_name, :theater, :city, :time 
 end
